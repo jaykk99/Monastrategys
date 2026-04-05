8fd005f1c00ff3b4e1da119678acacc1ac458ef6
jaykk99 <jayomer1234@gmail.com>
2026-04-04 21:33:09 -0500

    Replaced corrupted App.tsx with a working React component for the trading strategies dashboard

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div id="app">
+            <h1>Monastrategys - Trading Strategies</h1>
+        </div>
+    );
+};
+
+export default App;
